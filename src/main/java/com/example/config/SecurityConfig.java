1ebe67e60d8c281ca03cb0e535f62dd6906d7b21
sampson.obenembot <sampson.obenembot@sgits.co.za>
2024-02-20 23:51:09 +0200

    init

--- src/main/java/com/example/config/SecurityConfig.java
+++ src/main/java/com/example/config/SecurityConfig.java
@@ -25,18 +25,10 @@ public class SecurityConfig {
     @Autowired
     private JwtAuthFilter authFilter;
 
+    private String[] allow = {"/v2/api-docs", "/swagger-ui/"};
+
     @Bean
-    //authentication
     public UserDetailsService userDetailsService() {
-//        UserDetails admin = User.withUsername("Basant")
-//                .password(encoder.encode("Pwd1"))
-//                .roles("ADMIN")
-//                .build();
-//        UserDetails user = User.withUsername("John")
-//                .password(encoder.encode("Pwd2"))
-//                .roles("USER","ADMIN","HR")
-//                .build();
-//        return new InMemoryUserDetailsManager(admin, user);
         return new UserInfoUserDetailsService();
     }
 
@@ -45,6 +37,7 @@ public class SecurityConfig {
         return http.csrf().disable()
                 .authorizeHttpRequests()
                 .requestMatchers("/products/new-user","/products/authenticate").permitAll()
+                .requestMatchers(allow).permitAll()
                 .and()
                 .authorizeHttpRequests().requestMatchers("/products/**")
                 .authenticated().and()
